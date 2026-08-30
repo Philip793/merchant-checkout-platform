373ed194ced3779a3f759b90604bc7f81ea7fc77
Philip <phil.christov@gmail.com>
2026-08-30 21:19:17 +1000

    fix provider hierarchy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
 import App from "./App.js";
 import reportWebVitals from "./reportWebVitals.js";
-import { CartProvider } from "./context/CartContext.js";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root"),
+);
+
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <App />
   </React.StrictMode>,
 );
 
